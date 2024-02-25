b15cdb42ae04e95592f631d6158b78e838102ba7
kyle <kp3crt@bolton.ac.uk>
2024-02-25 09:31:16 +0000

    Additional progress made on the menu so that sub menus can be used to allow for more flexability

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # start of the programme to help debug issues with a dell computer
 print("Welcome to the software!")
-# making the first menu that the user will see
+# code used from https://medium.com/@dghadge2002/building-a-command-line-menu-in-python-exploring-different-applications-f970a61e6412
+# making the menu that the user will see
 while True:
         print("Does the computer turn on?")
         print("Press 1 for Yes")
@@ -8,16 +9,23 @@ while True:
         print("Press 0 to exit programme\n")
         choice = int(input())
         if choice == 1:
-            print("Yes\n")
-        else:
-            print("Why did you press that :/")
+            print("Is anything displayed on the screen?\n")
+            print("Press 1 if anything is displayed")
+            print("Press 2 if nothing is displayed\n")
+            choice = int(input())
+            if choice == 1:
+                print("Working")
+                break
+            elif choice == 2:
+                print("Not working")
+                break
+        elif choice == 2:
+            print("No")
             break
-        #if choice == "1":
-            #print("Yes")
-        #elif choice == "2":
-            #print("No")
-        #elif choice == "0":
-            #print("Exit")
-        #else:
-            #print("Invalid choice!")
-# end of the fist menu
+        elif choice == 0:
+            print("Exit")
+            break
+        else:
+            print("Invalid choice!")
+            break
+# end of the menu

cda8dc999c74dc552194a0d8909ad064da45da2d
kyle <kp3crt@bolton.ac.uk>
2024-02-25 08:37:47 +0000

    Making progress on the code :) the menu is starting to come together

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # start of the programme to help debug issues with a dell computer
+print("Welcome to the software!")
+# making the first menu that the user will see
+while True:
+        print("Does the computer turn on?")
+        print("Press 1 for Yes")
+        print("Press 2 for No")
+        print("Press 0 to exit programme\n")
+        choice = int(input())
+        if choice == 1:
+            print("Yes\n")
+        else:
+            print("Why did you press that :/")
+            break
+        #if choice == "1":
+            #print("Yes")
+        #elif choice == "2":
+            #print("No")
+        #elif choice == "0":
+            #print("Exit")
+        #else:
+            #print("Invalid choice!")
+# end of the fist menu
